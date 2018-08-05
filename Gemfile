7e378b5779fd7e86ae3dfa24e1823c05fa9353bb
Kuba Bogaczewicz <kuba.bogaczewicz@gmail.com>
2018-08-05 19:41:33 +0200

    Support mp4, libs update, refactoring

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
-gem 'exif'
-gem 'mini_exiftool'
-gem 'tty-prompt'
-gem 'pastel'
+gem "exif", "~> 2.0"
+gem "mini_exiftool", "~> 2.0"
+gem "pastel"
+gem "tty-prompt"
--- organize.rb
+++ organize.rb
@@ -1,41 +1,41 @@
 #!/usr/bin/env ruby
 
-require 'optparse'
+require "exif" # fast exif for images
+require "mini_exiftool" # slow, cmd exiftool wrapper, supports movies
 
-require 'exif' # fast exif for images
-require 'mini_exiftool' # slow, cmd exiftool wrapper, supports movies
-
-require 'tty-prompt'
-require 'pastel'
-require 'fileutils'
+require "fileutils"
+require "find"
+require "optparse"
+require "pastel"
+require "shellwords"
+require "tty-prompt"
 
 pastel = Pastel.new(enabled: $stdout.tty?, eachline: "\n")
-verbose  = pastel.yellow.detach
 
 prompt = TTY::Prompt.new
 
 options = {
   verbose: false,
   dry_run: false,
-  enforce_gps: false,
-  move: false
+  enforce_gps: true,
 }
 OptionParser.new do |opt|
   opt.banner = "Usage: organize.rb [options] src dst"
 
-  opt.on('-v', '--[no-]verbose', 'Run verbosely') { |v| options[:verbose] = v }
-  opt.on('--dry-run', 'Run whole program but do not actually perform any changes to files. Implies --verbose.') do
+  opt.on("-v", "--[no-]verbose", "Run verbosely") { |v| options[:verbose] = v }
+  opt.on("--dry-run", "Run whole program but do not actually perform any changes to files. Implies --verbose.") do
     options[:dry_run] = true
     options[:verbose] = true
   end
-  opt.on('--[no-]enforce-gps', 'Abort if any photo misses GPS data') { |v| options[:enforce_gps] = v }
-  opt.on_tail('-h', '--help', 'Prints help') { puts opt.help(); exit }
+  opt.on("--[no-]enforce-gps", "Abort if any photo misses GPS data") { |v| options[:enforce_gps] = v }
+  opt.on_tail("-h", "--help", "Prints help") { puts opt.help(); exit }
 end.parse!
 
 if ARGV.size != 2
-  prompt.error("Wrong number of arguments, expected 2 (src and dst), got #{ARGV.size}: #{ARGV.join(' ')}")
+  prompt.error("Wrong number of arguments, expected 2 (src and dst), got #{ARGV.size}: #{ARGV.join(" ")}")
   exit 1
 end
+
 src = ARGV[0]
 dst = ARGV[1]
 
@@ -43,6 +43,7 @@ unless Dir.exist?(src)
   prompt.error("#{src} is not a directory")
   exit 1
 end
+
 unless Dir.exist?(dst)
   if File.exist?(dst)
     prompt.error("#{dst} is not a directory. Aborting")
@@ -55,30 +56,37 @@ unless Dir.exist?(dst)
   end
 end
 
+class Media
+  attr_reader :filename
 
-class Image
-  attr_reader :filename, :exif
-
-  def initialize(filename)
-    @filename = filename
-    @exif = Exif::Data.new(filename)
-  end
-
-  def has_gps?
-    !@exif[:gps_latitude].nil?
-  end
-
-  def date_time
-    @exif[:date_time_original]&.getutc
-  end
-
-  def ext
-    '.jpg'
+  def extname
+    File.extname(@filename).downcase
   end
 end
 
-class Movie
-  attr_reader :filename, :exif
+class Image < Media
+  def self.===(path)
+    /jpe?g$/i === path
+  end
+
+  def initialize(filename)
+    @filename = filename
+    @exif = Exif::Data.new(File.open(filename))
+  end
+
+  def has_gps?
+    !@exif[:gps].empty?
+  end
+
+  def create_time
+    Time.strptime(@exif.date_time_original, "%Y:%m:%d %H:%M:%S").getutc
+  end
+end
+
+class Movie < Media
+  def self.===(path)
+    /(mov|mp4)$/i === path
+  end
 
   def initialize(filename)
     @filename = filename
@@ -89,70 +97,63 @@ class Movie
     true # don't care about geolocation in movies cause I lack tools to add them manually
   end
 
-  def date_time
-    @exif.creation_date || @exif.media_create_date || @exif.file_modify_date || File::Stat.new(filename).mtime
-  end
-
-  def ext
-    '.mov'
+  def create_time
+    (@exif[:date_time_original] ||
+     @exif[:create_date] ||
+     @exif[:media_create_date] ||
+     @exif[:file_modify_date]).getutc
   end
 end
 
-def walk(start)
-  result = Array.new
-  walk_recursive = lambda do |start, acc|
-    Dir.foreach(start) do |uri|
-      path = File.join(start, uri)
-      if %w(. ..).include?(uri)
-        next
-      elsif File.directory?(path)
-        walk_recursive.call(path, acc)
-      else
-        acc << path
-      end
-    end
-  end
-  walk_recursive.call(start, result)
-  result
+def is_media?(path)
+  [Movie, Image].any? { |c| c === path }
 end
 
-files = walk(src)
+def create_media(path)
+  case path
+  when Image
+    Image.new(path)
+  when Movie
+    Movie.new(path)
+  end
+end
 
-movies = files
-  .select { |filename| /mov$/i === filename }
-  .map { |filename| Movie.new(filename) }
+files = Find.find(src)
+  .select { |path| File.file?(path) }
+  .select { |path| is_media?(path) }
+  .map { |path| create_media(path) }
 
-photos = files
-  .select { |filename| /jpe?g$/i === filename }
-  .map { |filename| Image.new(filename) }
+files_without_gps = files.select { |f| !f.has_gps? }
 
-photos_without_gps = photos.select { |photo| !photo.has_gps? }
-
-unless photos_without_gps.empty?
-  pluralized_images = photos_without_gps.size == 1 ? "1 photo is" : "#{photos_without_gps.size} photos are"
+unless files_without_gps.empty?
+  pluralized_images = files_without_gps.size == 1 ? "1 photo is" : "#{files_without_gps.size} photos are"
   prompt.warn("#{pluralized_images} missing gps data")
-  photos_without_gps.each { |img| prompt.warn(img.filename) } if options[:verbose]
+  files_without_gps.each { |img| prompt.warn(img.filename) } if options[:verbose]
   if options[:enforce_gps]
-    prompt.error('Aborting')
+    prompt.error("Aborting")
     exit
-  elsif options[:enforce_gps].nil? && prompt.no?('Do you want to continue?')
-    prompt.error('Aborting')
+  elsif prompt.no?("Do you want to continue?")
+    prompt.error("Aborting")
     exit
   end
 end
 
-files = photos + movies
-files.group_by { |any_image| any_image.date_time.year }.each do |year, yearly|
+def fast_system_copy(src, dst, noop, verbose)
+  puts "Copying #{src.shellescape} -> #{dst.shellescape}" if verbose
+  `cp -pc #{src.shellescape} #{dst.shellescape}` unless noop
+end
+
+files.group_by { |media| media.create_time.year }.each do |year, yearly|
   output_year = File.join(dst, year.to_s)
   FileUtils.mkdir(output_year, noop: options[:dry_run], verbose: options[:verbose]) unless Dir.exist?(output_year)
   yearly.each do |image|
-    datetime = image.date_time.strftime("%Y-%m-%d %H-%M-%S")
+    datetime = image.create_time.strftime("%Y-%m-%d %H-%M-%S")
     idx = 1
-    output_filename = datetime + image.ext
+    output_filename = datetime + image.extname
     while File.exist?(File.join(output_year, output_filename))
       output_filename = [datetime, " (#{idx})", image.ext].join
       idx += 1
     end
-    FileUtils.cp(image.filename, File.join(output_year, output_filename), noop: options[:dry_run], verbose: options[:verbose])
+    fast_system_copy(image.filename, File.join(output_year, output_filename), options[:dry_run], options[:verbose])
   end
 end
